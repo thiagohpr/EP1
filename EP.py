c88cedf921b0b9146c9eab3a192e629c0ec49fd5
FabrizioMilan <fabriziosm@al.insper.edu.br>
2020-03-31 15:15:23 -0300

    Atualização do tempo

--- EP.py
+++ EP.py
@@ -1,4 +1,5 @@
 import random
+import time
 craps=True
 fichas=100
 
@@ -74,9 +75,15 @@ while craps:
                 plbc=False
 
 
-        print ("Primeiro dado: {}".format (dado11))
-        print ("Segundo dado: {}".format (dado12))
-        print ("Soma de {}.".format (soma1))
+        print ("Primeiro dado: ")
+        time.sleep (3)
+        print (dado11)
+        time.sleep (1)
+        print ("Segundo dado: ")
+        time.sleep (3)
+        print (dado12)
+        time.sleep (1)
+        print ("A soma é de {}" .format(soma1))
 
         if fie>0:
             if soma1==3 or soma1==4 or soma1==9 or soma1==10 or soma1==11:
@@ -90,21 +97,21 @@ while craps:
                 print ("Você recuperou sua aposta e ganhou {} no field.".format (3*fie))
             else:
                 print ("Você perdeu no field.")
-
+        time.sleep (2)
         if any>0:
             if soma1==2 or soma1==3 or soma1==12:
                 fichas=fichas+8*any 
                 print ("Craps! Você recuperou sua aposta e ganhou {} no any craps.".format (7*any))
             else:
                 print ("Você perdeu no any craps")
-            
+        time.sleep (2)    
         if twe>0:
             if soma1==12:
                 fichas=fichas+31*twe
                 print ("JACKPOT! Você recuperou sua aposta e ganhou {} no twelve!".format (30*twe))
             else:
                 print ("Você perdeu no twelve.")
-        
+        time.sleep (2)
         if plb>0:
             if soma1==7 or soma1==11:
                 fichas=fichas+2*plb
@@ -158,9 +165,15 @@ while craps:
                         else:
                             twec=False
 
-                    print ("Primeiro dado: {}".format (dado21))
-                    print ("Segundo dado: {}".format (dado22))
-                    print ("Soma de {}.".format (soma2))
+                    print ("Primeiro dado: ")
+                    time.sleep (3)
+                    print (dado21)
+                    time.sleep (1)
+                    print ("Segundo dado: ")
+                    time.sleep (3)
+                    print (dado22)
+                    time.sleep (1)
+                    print ("A soma é de {}.".format (soma2))
 
                     if fie>0:
                         if soma1==3 or soma1==4 or soma1==9 or soma1==10 or soma1==11:
@@ -174,14 +187,14 @@ while craps:
                             print ("Você recuperou sua aposta e ganhou {} no field.".format (3*fie))
                         else:
                             print ("Você perdeu no field.")
-
+                    time.sleep (2)
                     if any>0:
                         if soma1==2 or soma1==3 or soma1==12:
                             fichas=fichas+8*any 
                             print ("Craps! Você recuperou sua aposta e ganhou {} no any craps.".format (7*any))
                         else:
                             print ("Você perdeu no any craps")
-            
+                    time.sleep (2)
                     if twe>0:
                         if soma1==12:
                             fichas=fichas+31*twe
