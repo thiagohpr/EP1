b607a883576cbcec6710eb99711d964d1da174a3
Your Name <thiagohpr@al.insper.edu.br>
2020-03-30 21:34:14 -0300

    inicio do programa, tirar duvidas no atendimento

--- EP.py
+++ EP.py
@@ -1,2 +1,11 @@
-#teste
-print ("hello world")
+import random
+craps=True
+fichas=100
+
+while craps:
+    jogo=str(input("Para jogar, digite J \nPara sair do jogo, digite S  "))
+    if jogo=="S":
+        craps=False
+    elif jogo=="J":
+        print ("Fase de Come Out")
+    
